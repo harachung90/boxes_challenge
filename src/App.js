ff041bcc10c315be7b655db9c184a6f7539c4a7f
Hara Chung <harachung90@gmail.com>
2022-12-25 17:41:53 +1100

    Sixth commit. Another way of writing toggle() function

--- src/App.js
+++ src/App.js
@@ -7,23 +7,29 @@ function App(props) {
     const [squares, setSquares] = React.useState(boxes)
 
     function toggle(id) {
-        setSquares(prevSquares => {
-            const newSquares = []
-            for(let i = 0; i < prevSquares.length; i++) {
-                const currentSquare = prevSquares[i]
-                if(currentSquare.id === id) {
-                    const updatedSquare = {
-                        ...currentSquare,
-                        on: !currentSquare.on
+        /*        setSquares(prevSquares => {
+                    const newSquares = []
+                    for(let i = 0; i < prevSquares.length; i++) {
+                        const currentSquare = prevSquares[i]
+                        if(currentSquare.id === id) {
+                            const updatedSquare = {
+                                ...currentSquare,
+                                on: !currentSquare.on
+                            }
+                            newSquares.push(updatedSquare)
+                        } else {
+                            newSquares.push(currentSquare)
+                        }
                     }
-                    newSquares.push(updatedSquare)
-                } else {
-                    newSquares.push(currentSquare)
-                }
-            }
-            return newSquares
+                    return newSquares
+                })*/
+        setSquares(prevSquares => {
+            return prevSquares.map((square) => {
+                return square.id === id ? {...square, on: !square.on} : square
+            })
         })
     }
+
 
     const squareElements = squares.map(square => (
         <Box
